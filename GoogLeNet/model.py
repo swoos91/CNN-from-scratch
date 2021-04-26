9a68daa3c2fc5620c077f37837e934f18053c024
seungwoo <mb910805@gmail.com>
2021-04-26 21:38:48 +0900

    Create model.py

---
+++ GoogLeNet/model.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+
+class conv_block(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(conv_block, self).__init__()
+        self.relu = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        return self.relu(self.batchnorm(self.conv(x)))
