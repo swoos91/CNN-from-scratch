3e637b29e986786b80fe66fd01bb6e55a8113c75
seungwoo <mb910805@gmail.com>
2021-04-27 22:16:33 +0900

    add InceptionAux class

--- GoogLeNet/model.py
+++ GoogLeNet/model.py
@@ -1,6 +1,25 @@
 import torch
 from torch import nn
 
+class InceptionAux(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(InceptionAux, self).__init__()
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.7)
+        self.pool = nn.AvgPool2d(kernel_size=5, stride=3)
+        self.conv = conv_block(in_channels, 128, kernel_size=1)
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
 
 class conv_block(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
